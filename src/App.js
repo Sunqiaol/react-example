a7f304c26128e29c900869f763e5c6d71ac88d32
johnnylaicode <jl8338@yahoo.com>
2022-03-16 10:03:52 -0400

    Updated file

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
-// import FolderComponent from './components/FolderComponent';
+import FolderComponent from './components/FolderComponent';
 // import TextAreaComponent from './components/TextAreaComponent';
 // import FormComponent from './components/FormComponent';
-import ApiDataComponent from './components/ApiDataComponent';
+// import ApiDataComponent from './components/ApiDataComponent';
 
 function App() {  // Need to replace the component with a specific one from the list below
   return (
     <div className="App">
-      <ApiDataComponent />
+      <FolderComponent inputData="This is props.data passing to component." />
     </div>
   );
 }
